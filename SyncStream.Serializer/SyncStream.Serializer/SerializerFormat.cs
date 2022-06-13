0c3090ac4201f7217ea93dde5fce97a134d50174
Travis Brown <travis.brown@nidavellir.in>
2022-06-13 13:27:45 -0400

    Adding format enum

---
+++ SyncStream.Serializer/SyncStream.Serializer/SerializerFormat.cs
@@ -0,0 +1,18 @@
+// Define our namespace
+namespace SyncStream.Serializer;
+
+/// <summary>
+/// This enum defines our serialization formats
+/// </summary>
+public enum SerializerFormat
+{
+    /// <summary>
+    /// This enum value defines JSON format
+    /// </summary>
+    Json,
+    
+    /// <summary>
+    /// This enum value defines XML format
+    /// </summary>
+    Xml
+}
